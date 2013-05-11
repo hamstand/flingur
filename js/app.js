c4d56cb3b753315d39b3aafd745805405ade5ffc
Graham Holt <graham@elmysterioso.com>
2013-05-11 22:32:10 +0100

    updated wip

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
-/*angular.module('flickrpickr', [], function($routeProvider) {
-  $routeProvider.when('/photo/:photoId', {
-    templateUrl: 'photo.html',
-    controller: photoSelectedCtrl
-  }).
-  otherwise({redirectTo: '/'});
-});*/
-
-
 var ffflingur = angular.module('ffflingur', ['ngResource']);
 
-
 ffflingur.factory('photoData',function($resource){
-    return $resource('js/flickr1.json',{ }, {
+    return $resource('js/flickr1.json',{}, {
     getData: {method:'GET', isArray: false}
     });
 })
