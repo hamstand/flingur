d4c74627ee9cc5826d7f17df516ff9d4be2a987a
Graham Holt <graham@elmysterioso.com>
2013-05-11 15:49:41 +0100

    putting some structure in there

---
+++ js/controllers.js
@@ -0,0 +1,27 @@
+
+
+var flickrURL = "http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=383d6452806c5b782b87e4ff292d20e7&nojsoncallback=1&per_page=50&page=1";
+	
+
+function photoCtrl($scope, $http) {
+	//use local till we sort out x-domain
+	$http.get("js/flickr1.json").success(function(data) {
+		
+		$scope.photos = data.photos;
+		/*
+			page: 1,
+    		pages: 10,
+    		perpage: 50,
+    		total: 500
+		*/
+
+		//return photo urls:
+		//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+		
+
+	});
+}
+
+
+    
+
