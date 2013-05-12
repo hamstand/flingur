14545a8a0eb08df9ff1f0c58cd484d7fa5947da2
Graham Holt <graham@elmysterioso.com>
2013-05-12 19:26:12 +0100

    refactoring with better code

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-//http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=237092eefc192e84aa7c30e357ad64bf&nojsoncallback=1&extras=tags&per_page=50&page=1
+//http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=237092eefc192e84aa7c30e357ad64bf&nojsoncallback=1&extras=tags&per_page=500&page=1
 
 var ffflingurApp = angular.module('ffflingurApp', ['ngResource']);
 
 ffflingurApp.factory('photoData',function($resource){
-    return $resource('js/flickr1.json',{}, {
-    getData: {method:'GET', isArray: false}
+    return $resource('js/page:pageId.json',{}, {
+    query: {method:'GET', params:{pageId:'currentPage'}, isArray: false}
     });
 })
     .config(function($routeProvider) {
         $routeProvider.when('/photo/:photoId', {
             templateUrl: 'photo.html',
-            controller: photoSelectedCtrl
+            controller: photoPickerCtrl   
         })
         .otherwise({
             redirectTo:'/',
+            controller: photoPickerCtrl   
         });
     });
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,80 @@
-function photoPickerCtrl($scope, photoData) {
-    photoData.get(function(data){
+function photoPickerCtrl($scope, photoData, $route, $routeParams) {
+   $scope.currentPage = 1;
+   console.log("Onon")
+   //$scope.photos = photoData.query({pageId: $scope.currentPage});
+   
+   // $scope.pages = $scope.photos.pages;
+    //$scope.currentPage = $scope.photos.page;
+
+
+    photoData.get({pageId: $scope.currentPage},function(data){
         $scope.photos = data.photos;
     });
-     
-}
 
-function photoSelectedCtrl ($scope, photoData, $routeParams) {
-    photoId = $routeParams.photoId;
-    
-    photoData.get(function(data){
-    	//guessing there's a better way to do this...
-        var phoLen = data.photos.photo.length;
+
+    //guessing there's a better way to do this...
+    $scope.viewPhoto = function(photoId) {
+       // console.log("VIEW",photoId)
+        var phoLen = $scope.photos.photo.length;
         for (var i = phoLen - 1; i >= 0; i--) {
-            if (data.photos.photo[i].id == photoId) {
+            if ($scope.photos.photo[i].id == photoId) {
                 break;
             }
+        }   
+        $scope.photo = $scope.photos.photo[i];
+    }
+
+
+    console.log("MEEE",$routeParams.photoId)
+    if ($routeParams.photoId) {
+        $scope.viewPhoto($routeParams.photoId)
+    }
+    /*photoData.get(function(data){
+        $scope.allData = data.photos;
+        console.log($scope.allData)
+        $scope.pages = 10;
+        perpage = 50;
+        $scope.currentPage = 1;
+
+
+
+
+
+    });*/
+
+    /*
+    var pagesShown = 1;
+    var pageSize = 5;
+    $scope.items = itemService.getAll();
+    $scope.itemsLimit = function() {
+        return pageSize * pagesShown;
+    };
+    $scope.hasMoreItemsToShow = function() {
+        return pagesShown < ($scope.items.length / pageSize);
+    };
+    $scope.showMoreItems = function() {
+        pagesShown = pagesShown + 1;         
+    };
+
+     $scope.pages = 10;
+        $scope.currentPage = 1;
+
+        console.log("SPG",$scope.currentPage);
+
+        $scope.prevPage = function () {
+            if ($scope.currentPage > 1) {
+                $scope.currentPage--;
+                console.log($scope.currentPage);
+            }
         }
-        $scope.photo = data.photos.photo[i];
-    });
-}   
+    
+        $scope.nextPage = function () {
+            if ($scope.currentPage < $scope.pages) {
+                $scope.currentPage++;
+                console.log($scope.currentPage);
+            }
+        }
+*/
+
+
+}
