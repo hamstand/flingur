7329316d5429d31b009576ab3b631f787b8560ec
Graham Holt <graham@elmysterioso.com>
2013-05-13 10:00:37 +0100

    fixed incorrect api key on singlephoto

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ ffflingurApp.factory('AllPhotos', function($resource) {
     return $resource('http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=237092eefc192e84aa7c30e357ad64bf&per_page=50&page=:pageId&extras=tags&nojsoncallback=1',{format:'json', jsoncallback: 'JSON_CALLBACK'},  {get: {method: 'JSONP', isArray: false}});
 })
 .factory('SinglePhoto', function($resource) {
-    return $resource('http://flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=147e1f183cb376d54f25dfe17e766a5e&photo_id=:photoId&format=json&nojsoncallback=1',{format:'json', jsoncallback: 'JSON_CALLBACK'},  {get: {method: 'JSONP', isArray: false}});
+    return $resource('http://flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=237092eefc192e84aa7c30e357ad64bf&photo_id=:photoId&format=json&nojsoncallback=1',{format:'json', jsoncallback: 'JSON_CALLBACK'},  {get: {method: 'JSONP', isArray: false}});
 })
 .config(function($routeProvider) {
     $routeProvider.when('/photo/:photoId', {
