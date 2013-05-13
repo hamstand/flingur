801c3ccb65ed4ce744112f1f3dd3955980d65572
Graham Holt <graham@elmysterioso.com>
2013-05-13 01:42:04 +0100

    nearly there

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-//http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=237092eefc192e84aa7c30e357ad64bf&nojsoncallback=1&extras=tags&per_page=500&page=1
 
 var ffflingurApp = angular.module('ffflingurApp', ['ngResource']);
 
-ffflingurApp.factory('photoData',function($resource){
-    return $resource('js/page:pageId.json',{}, {
-    query: {method:'GET', params:{pageId:'currentPage'}, isArray: false}
-    });
+ffflingurApp.factory('AllPhotos', function($resource) {
+    return $resource('http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&api_key=237092eefc192e84aa7c30e357ad64bf&nojsoncallback=1&per_page=50&page=:pageId&extras=tags',{format:'json', jsoncallback: 'JSON_CALLBACK'},  {get: {method: 'JSONP', isArray: false}});
 })
-    .config(function($routeProvider) {
-        $routeProvider.when('/photo/:photoId', {
-            templateUrl: 'photo.html',
-            controller: photoPickerCtrl   
-        })
-        .otherwise({
-            redirectTo:'/',
-            controller: photoPickerCtrl   
-        });
+.factory('SinglePhoto', function($resource) {
+    return $resource('http://flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=147e1f183cb376d54f25dfe17e766a5e&photo_id=:photoId&format=json&nojsoncallback=1',{format:'json', jsoncallback: 'JSON_CALLBACK'},  {get: {method: 'JSONP', isArray: false}});
+})
+.config(function($routeProvider) {
+    $routeProvider.when('/photo/:photoId', {
+        templateUrl: 'photo.html',
+        controller: 'PhotoSelectedCtrl'
+    })
+    .otherwise({
+        redirectTo:'/'
     });
+});
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,80 +1,32 @@
-function photoPickerCtrl($scope, photoData, $route, $routeParams) {
-   $scope.currentPage = 1;
-   console.log("Onon")
-   //$scope.photos = photoData.query({pageId: $scope.currentPage});
-   
-   // $scope.pages = $scope.photos.pages;
-    //$scope.currentPage = $scope.photos.page;
+ffflingurApp.controller('PhotoPickerCtrl', function($scope, AllPhotos) {
+    $scope.currentPage = 1;
 
+    $scope.getPhotos = function () {
+        AllPhotos.get({pageId: $scope.currentPage},function(data){
+            $scope.photos = data.photos;
+            $scope.pages = $scope.photos.pages;
+        });
+    }
 
-    photoData.get({pageId: $scope.currentPage},function(data){
-        $scope.photos = data.photos;
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 1) {
+            $scope.currentPage--;
+            $scope.getPhotos();
+        }
+    }
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pages) {
+            $scope.currentPage++;
+            $scope.getPhotos();
+        }
+    }
+
+    $scope.getPhotos();
+}).controller('PhotoSelectedCtrl', function($scope, SinglePhoto, $routeParams) {
+    var currPhoto = $routeParams.photoId;
+    SinglePhoto.get({photoId: currPhoto},function(data){
+        $scope.photo = data.photo;
+        $scope.imgURL = "http://farm"+$scope.photo.farm+".staticflickr.com/"+$scope.photo.server+"/"+$scope.photo.id+"_"+$scope.photo.secret+"_b.jpg";
     });
-
-
-    //guessing there's a better way to do this...
-    $scope.viewPhoto = function(photoId) {
-       // console.log("VIEW",photoId)
-        var phoLen = $scope.photos.photo.length;
-        for (var i = phoLen - 1; i >= 0; i--) {
-            if ($scope.photos.photo[i].id == photoId) {
-                break;
-            }
-        }   
-        $scope.photo = $scope.photos.photo[i];
-    }
-
-
-    console.log("MEEE",$routeParams.photoId)
-    if ($routeParams.photoId) {
-        $scope.viewPhoto($routeParams.photoId)
-    }
-    /*photoData.get(function(data){
-        $scope.allData = data.photos;
-        console.log($scope.allData)
-        $scope.pages = 10;
-        perpage = 50;
-        $scope.currentPage = 1;
-
-
-
-
-
-    });*/
-
-    /*
-    var pagesShown = 1;
-    var pageSize = 5;
-    $scope.items = itemService.getAll();
-    $scope.itemsLimit = function() {
-        return pageSize * pagesShown;
-    };
-    $scope.hasMoreItemsToShow = function() {
-        return pagesShown < ($scope.items.length / pageSize);
-    };
-    $scope.showMoreItems = function() {
-        pagesShown = pagesShown + 1;         
-    };
-
-     $scope.pages = 10;
-        $scope.currentPage = 1;
-
-        console.log("SPG",$scope.currentPage);
-
-        $scope.prevPage = function () {
-            if ($scope.currentPage > 1) {
-                $scope.currentPage--;
-                console.log($scope.currentPage);
-            }
-        }
-    
-        $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pages) {
-                $scope.currentPage++;
-                console.log($scope.currentPage);
-            }
-        }
-*/
-
-
-}
+});
